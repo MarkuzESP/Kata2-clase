11ecd9eb891075782d3def65c7ac58cac8d19d23
MarkuzESP <marcosdiazgil@gmail.com>
2021-12-28 22:58:39 +0000

    Kata 2 Versión 3
    Se integra una clase independiente para tratar los objetos Histogram.

--- Kata2/src/kata2/Kata2.java
+++ Kata2/src/kata2/Kata2.java
@@ -9,17 +9,16 @@ public class Kata2 {
 // En este ejercicio se hará uso de los wrappers: clases que permiten manipular
 // tipos primitivos (como int) como si de objetos se tratasen.
     public static void main(String[] args) {
-        int elementos[] = new int []{1,4,0,4,13,2,29,7,29,5,10,8,29,14,15}; 
-        Map <Integer, Integer> histograma = new HashMap<>();
-        // Para este enunciado se utiliza una estructura foreach
-        for (int i : elementos){
-            // Operador ternario. Si la clave no está, la anota
-            // Si la clave está registrada, incrementa su valor.
-            histograma.put(i, histograma.containsKey(i) ? histograma.get(i)+1: 1);
-        }
+        int data[] = new int []{1,4,0,4,13,2,29,7,29,5,10,8,29,14,15}; 
+        Histogram histo = new Histogram(data);
+        // Ahora el histograma se prepara desde la clase Histogram. 
+        // Antes se hacía todo en la propia clase del main
+        Map<Integer, Integer> histogr = histo.getHistogram();
         
-        for (Integer clave : histograma.keySet()){
-            System.out.println(clave + "==>" + histograma.get(clave));
+        // Se itera en el mapa recién creado y se muestran las keys.
+        for (Integer key : histogr.keySet()){
+            System.out.println(key + "==>" + histogr.get(key));
         }
-    }
+        // El resultado debe ser el mismo que en el ejercicio anterior
+    }  
 }
